4d6fac5391b1d9c8ba1bbc65a96f84e8e61007f5
luke-e-gibson <luke.e.gibson@outlook.com>
2025-01-16 22:43:53 +1100

    update drawjs version

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,37 @@ import {
   CanvasDebugButton,
   PenColorChanger,
   PenWidthChanger,
+  DrawProvider,
 } from "@luke-e-gibson/drawjs";
 
 function App() {
+
   return (
     <>
-      <h1 className="text-center text-2xl">Drawjs Demo</h1>
-      <div className="container mx-auto p-4" style={{height: `${window.innerHeight - 100}px`}}>
-        <div className="flex justify-between py-3">
-          <div className="flex items-center">
-            <label htmlFor="colorpicker" className="px-1">Color: </label><PenColorChanger id="colorpicker"/>
+      <DrawProvider>
+        <h1 className="text-center text-2xl">Drawjs Demo</h1>
+        <div
+          className="container mx-auto p-4"
+          style={{ height: `${window.innerHeight - 100}px` }}
+        >
+          <div className="flex justify-between py-3">
+            <div className="flex items-center">
+              <label htmlFor="colorpicker" className="px-1">
+                Color:
+              </label>
+              <PenColorChanger id="colorpicker" />
+            </div>
+            <div className="flex items-center">
+              <label htmlFor="width" className="px-2">
+                Pen Width:
+              </label>
+              <PenWidthChanger id="width" className="p-1 border rounded-md" />
+            </div>
+            <CanvasDebugButton className="p-2 border rounded-md" />
           </div>
-          <div className="flex items-center">
-            <label htmlFor="width" className="px-2">Pen Width: </label>
-            <PenWidthChanger id="width" className="p-1 border rounded-md"/>
-          </div>
-          <CanvasDebugButton className="p-2 border rounded-md"/>
+          <Canvas className="border rounded-md w-full h-full" />
         </div>
-        <Canvas className="border rounded-md w-full h-full" />
-      </div>
+      </DrawProvider>
     </>
   );
 }
